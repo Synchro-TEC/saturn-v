ea627653c0d949e96c34433f2985c2730aea8f4e
Marcio Gasparotto <mgasparotto@gmail.com>
2017-06-29 22:29:56 -0300

    wip

--- grunt/exec.js
+++ grunt/exec.js
@@ -1,6 +1,8 @@
 module.exports = {
   cp: {
-    command: 'cp css/saturn-v.css ../the-oracle/src/assets/css/saturn-v.css',
+    // command: 'cp css/saturn-v.css ../site-apollo-11/dist/saturn-v.css',
     // command: 'cp css/saturn-v.css ../site-saturn-v/dist/saturn-v.css'
+    // command: 'cp css/saturn-v.css ../guide-saturn-v/static/css/saturn-v.css',
+    command: 'cp css/saturn-v.css ../guide-saturn-v/public/css/saturn-v.css',
   }
 };
